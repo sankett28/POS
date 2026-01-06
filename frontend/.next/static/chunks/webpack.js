398b01beef80b95ccb74adde2791185eaab226bf
sankett28 <sanketpimprikar@gmail.com>
2026-01-06 16:54:57 +0530

    some normal changes

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dde420386ecd3745"; }
+/******/ 		__webpack_require__.h = function() { return "00d6aebea12935bc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
